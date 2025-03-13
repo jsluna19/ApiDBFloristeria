1e46a755b342b561254cbc6cb24d1d95ee323cce
jsluna19 <Juan.luna@tau.usbmed.edu.co>
2025-03-13 16:13:16 -0500

    se esta organizando Order en domain

--- src/main/java/com/Api_DB_Floristeria_Amistad/domain/dto/Client.java
+++ src/main/java/com/Api_DB_Floristeria_Amistad/domain/dto/Client.java
@@ -12,6 +12,4 @@ public class Client {
     private Order order;
     private Addressee addressee;
 
-
-
 }
--- src/main/java/com/Api_DB_Floristeria_Amistad/domain/dto/FlowerArrangement.java
+++ src/main/java/com/Api_DB_Floristeria_Amistad/domain/dto/FlowerArrangement.java
@@ -8,39 +8,6 @@ public class FlowerArrangement {
     private String description;
     private String attachments; //anexos
     private Double price;
-
-    // Getters and Setters
-    public Integer getArregloID() {
-        return arregloID;
-    }
-    public void setArregloID(Integer arregloID) {
-        this.arregloID = arregloID;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    public String getAttachments() {
-        return attachments;
-    }
-    public void setAttachments(String attachments) {
-        this.attachments = attachments;
-    }
-    public Double getPrice() {
-        return price;
-    }
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-
+    private OrderDetail orderDetail;
 
 }
